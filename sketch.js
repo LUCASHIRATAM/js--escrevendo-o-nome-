b252d86ebc1caecf96c5c02d3c6b3594fba5dd55
LUCASHIRATAM <hiratalucas23@gmail.com>
2024-05-27 08:17:29 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,10 @@
 function setup() {
     createCanvas(600, 600);
     background("black");
-    
   }
-  
   function draw() {
     stroke("blue");
     fill("red");
-    
     if(mouseIsPressed){
    rect(mouseX,mouseY,20,35);
   }
